c455161fc310d8e88f3309f1f4e2e6b5e22159a8
Sylvain Boily <sylvain.boily@gmail.com>
2015-07-27 17:52:51 -0400

    the OS should control the number of FD not the application

--- hpsa/cassandra-reaper.postInstallScript.sh
+++ hpsa/cassandra-reaper.postInstallScript.sh
@@ -3,5 +3,3 @@ chmod +x /etc/init.d/cassandra-reaper
 service cassandra-reaper start
 chkconfig --add cassandra-reaper
 chkconfig cassandra-reaper on
-echo "*           soft    nofile          8192" >> /etc/security/limits.conf
-echo "*           hard    nofile          16384" >> /etc/security/limits.conf
